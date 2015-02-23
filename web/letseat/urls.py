c6f9f0a87e55c5860e596f81acafa3fb9813bbd5
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-02-23 15:17:44 +0200

    Edit profile page url fixed

--- web/letseat/urls.py
+++ web/letseat/urls.py
@@ -6,12 +6,12 @@ from api.views import user_views
 urlpatterns = patterns('',
 
     #Just for testing urls
-    url(r'^test/', user_views.test, name='test'),
+    url(r'^test/$', user_views.test, name='test'),
 
-    url(r'^register/', user_views.registration_view, name='register'),
-    url(r'^profile/(?P<username>\w+)/', user_views.profile, name='profile'),
-    url(r'^profile/(?P<username>\+w)/edit', user_views.edit, name='edit'),
-    url(r'^login/', user_views.login_view, name='login'),
+    url(r'^register/$', user_views.registration_view, name='register'),
+    url(r'^profile/(?P<username>\w+)/$', user_views.profile, name='profile'),
+    url(r'^profile/(?P<username>\w+)/edit/$', user_views.edit, name='edit'),
+    url(r'^login/$', user_views.login_view, name='login'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
