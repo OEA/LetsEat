8ba0d3ea1ec153ada38aba9ffa5ed8388c8a0847
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-02-23 15:08:13 +0200

    Profile page url fixed

--- web/letseat/urls.py
+++ web/letseat/urls.py
@@ -9,7 +9,8 @@ urlpatterns = patterns('',
     url(r'^test/', user_views.test, name='test'),
 
     url(r'^register/', user_views.registration_view, name='register'),
-    url(r'^profile/', user_views.profile, name='profile'),
+    url(r'^profile/(?P<username>\w+)/', user_views.profile, name='profile'),
+    url(r'^profile/(?P<username>\+w)/edit', user_views.edit, name='edit'),
     url(r'^login/', user_views.login_view, name='login'),
 
     url(r'^admin/', include(admin.site.urls)),
