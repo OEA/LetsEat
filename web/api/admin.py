4f628564a1634c3c5ee3c89ae46c492740810394
bahadirk <bahadirk@users.noreply.github.com>
2015-03-09 14:44:34 +0200

    Fixed admin page

--- web/api/admin.py
+++ web/api/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from .models.user import User
 
 class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'surname', 'email', 'username')
+
     fieldsets = [
-        ('Personal Infromation',    {'fields': ['photo', 'name', 'surname', 'password', 'username', 'is_admin', 'is_active']}),
-        ('Contact information',     {'fields': ['email']}),
+        ('Personal Infromation',    {'fields': ['photo', 'name', 'surname', 'password', 'username']}),
+        ('Contact Information',     {'fields': ['email']}),
+        ('Admin Privilage',         {'fields': ['is_admin', 'is_active']}),
     ]
 
 
