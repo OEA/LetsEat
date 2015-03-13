7e763427858463537b8c63c2631d131a139f833b
bahadirk <bahadirk@users.noreply.github.com>
2015-03-13 18:07:31 +0200

    Change login page to the homepage Remove /login url pattern and add homepage url pattern

--- web/letseat/urls.py
+++ web/letseat/urls.py
@@ -10,12 +10,12 @@ urlpatterns = patterns('',
     #Just for testing urls
     url(r'^test/$', user_views.test, name='test'),
 
+    url(r'^$', user_views_.login_view, name='login'),
     url(r'^api/register/$', user_views.registration_view, name='api_register'),
     url(r'^api/profile/$', user_views.user_profile, name='api_user_profile'),
     url(r'^api/profile/(?P<username>\w+)/$', user_views.profile, name='api_profile'),
     url(r'^api/profile/(?P<username>\w+)/edit/$', user_views.edit, name='api_edit'),
     url(r'^api/login/$', user_views.login_view, name='api_login'),
-    url(r'^login/$', user_views_.login_view, name='login'),
     url(r'^logout/$', user_views.logout, name='logout'),
     url(r'^register/$', user_views_.registration_view, name='register'),
     url(r'^profile/(?P<username>\w+)/$', user_views_.profile, name='profile'),
