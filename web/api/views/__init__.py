2621efa63bd83db3e6414545e64646fb2b4ad7ff
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-03-21 21:32:15 +0200

    New api urls added

--- web/api/views/__init__.py
+++ web/api/views/__init__.py
@@ -1,4 +1,5 @@
 __author__ = 'Hakan Uyumaz & Burak Atalay'
 
 from .user_views import registration_view, login_view
-from .friendship_views import search_user
+from .friendship_views import search_user, accept_friend_request, get_friend_list, reject_friend_request, \
+    send_friend_request
