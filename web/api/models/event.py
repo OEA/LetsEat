b1bd1e38586f52239178d5be4701d9ab1dc5522d
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-03-23 23:04:54 +0200

    Api get owned events method added

--- web/api/models/event.py
+++ web/api/models/event.py
@@ -15,3 +15,7 @@ class Event(models.Model):
     restaurant = models.ForeignKey(Restaurant, related_name='restaurant')
     participants = models.ManyToManyField(Restaurant, related_name='participants', null=True, blank=True)
     joinable = models.BooleanField('Joinable', default=False)
+
+    def get_type_symbol(self, type):
+        TYPE_LABELS_REVERSE = {x: y for x, y in self.TYPE_LABELS}
+        return TYPE_LABELS_REVERSE[type]
--- web/api/views/event_views.py
+++ web/api/views/event_views.py
@@ -53,3 +53,37 @@ def invite_event(request):
         responseJSON["status"] = "failed"
         responseJSON["message"] = "No request found."
     return HttpResponse(json.dumps(responseJSON), content_type="application/json")
+
+
+def get_owned_events(request):
+    responseJSON = {}
+    if request.method == "POST":
+        user = get_object_or_404(User, username=request.POST["username"])
+        events = Event.objects.filter(owner=user)
+        responseJSON["events"] = []
+        for event in events:
+            eventJSON = {}
+            eventJSON["name"] = event.name
+            restaurantJSON = {}
+            restaurantJSON["name"] = event.restaurant.name
+            restaurantJSON["latitude"] = event.restaurant.latitude
+            restaurantJSON["longitude"] = event.restaurant.longitude
+            eventJSON["restaurant"] = restaurantJSON
+            eventJSON["time"] = event.time
+            eventJSON["type"] = event.get_type_symbol(event.type)
+            eventJSON["participants"] = []
+            for participant in event.participants.all():
+                participantJSON = {}
+                participantJSON["name"] = participant.name
+                participantJSON["surname"] = participant.surname
+                participantJSON["username"] = participant.username
+                participantJSON["email"] = participant.email
+                eventJSON["participants"].append(participantJSON)
+            eventJSON["joinable"] = event.joinable
+            responseJSON["events"].append(eventJSON)
+        responseJSON["status"] = "success"
+        responseJSON["message"] = "Events found."
+    else:
+        responseJSON["status"] = "failed"
+        responseJSON["message"] = "No request found."
+    return HttpResponse(json.dumps(responseJSON))
