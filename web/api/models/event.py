6d3aaed1a7cf1bdceee594596da3c44eb2dd81dd
bahadirk <bahadirk@users.noreply.github.com>
2015-03-29 13:18:27 +0300

    Add auto_now_add in event time field

--- web/api/models/event.py
+++ web/api/models/event.py
@@ -10,7 +10,7 @@ class Event(models.Model):
     TYPE_LABELS = (('D', 'Dinning'), ('M', 'Meal'))
     owner = models.ForeignKey(User, related_name="owner")
     name = models.CharField('Name', max_length=50, unique=False, null=True, blank=True)
-    time = models.DateTimeField('Time', auto_now_add=False, null=False, blank=False)
+    time = models.DateTimeField('Time', auto_now_add=True, null=False, blank=False)
     type = models.CharField('Type', max_length=50, choices=TYPE_LABELS)
     restaurant = models.ForeignKey(Restaurant, related_name='restaurant')
     participants = models.ManyToManyField(Restaurant, related_name='participants', null=True, blank=True)
