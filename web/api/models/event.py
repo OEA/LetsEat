33f763f31ce8a6424bb6dab95d5fc854a6ba44f8
Hakan Uyumaz <hakanuyumaz@gmail.com>
2015-03-30 15:33:32 +0300

    Create event fixed

--- web/api/models/event.py
+++ web/api/models/event.py
@@ -7,6 +7,7 @@ from .user import User
 
 class Event(models.Model):
     TYPE_LABELS = (('D', 'Dinning'), ('M', 'Meal'))
+    TYPE_LABELS_REVERSE = {y: x for x, y in self.TYPE_LABELS}
     owner = models.ForeignKey(User, related_name="owner")
     name = models.CharField('Name', max_length=50, unique=False, null=True, blank=True)
     start_time = models.DateTimeField('Start_Time', auto_now_add=True, null=True, blank=True)
@@ -15,6 +16,3 @@ class Event(models.Model):
     participants = models.ManyToManyField(User, related_name='participants', null=True, blank=True)
     joinable = models.BooleanField('Joinable', default=False)
 
-    def get_type_symbol(self, type):
-        TYPE_LABELS_REVERSE = {x: y for x, y in self.TYPE_LABELS}
-        return TYPE_LABELS_REVERSE[type]
