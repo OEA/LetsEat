427dc2863d1723953a67c7d86a70dd29a3e8cfc1
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-04-06 19:07:30 +0300

    Done refactoring and added friend list count

--- web/frontend/views/home_view.py
+++ web/frontend/views/home_view.py
@@ -6,19 +6,40 @@ import json
 
 from django.shortcuts import render, redirect
 
+
+def get_friend_requests(user):
+    params = urllib.parse.urlencode({'username': user.username})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "application/json"}
+    connection = http.client.HTTPConnection('127.0.0.1', 8000)
+    connection.request("POST", "/api/get_friend_requests/", params, headers)
+    friend_request_response = connection.getresponse()
+    friend_request_json_data = friend_request_response.read()
+    friend_request_data = json.loads(friend_request_json_data.decode("utf-8"))
+    friend_request_list = friend_request_data["senders"]
+    return friend_request_list
+
+
+def get_friends_count(user):
+    params = urllib.parse.urlencode({'username': user.username})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "application/json"}
+    connection = http.client.HTTPConnection('127.0.0.1', 8000)
+    connection.request("POST", "/api/get_friends/", params, headers)
+    friend_list_response = connection.getresponse()
+    friend_list_json_data = friend_list_response.read()
+    friend_list_data = json.loads(friend_list_json_data.decode("utf-8"))
+    friend_list = friend_list_data["friends"]
+
+    return len(friend_list)
+
+
 def homepage(request):
     context = None
     if request.user.is_authenticated():
         user = request.user
-        params = urllib.parse.urlencode({'username': user.username})
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "application/json"}
-        connection = http.client.HTTPConnection('127.0.0.1',8000)
-        connection.request("POST", "/api/get_friend_requests/", params, headers)
-        friend_request_response = connection.getresponse()
-        friend_request_json_data = friend_request_response.read()
-        friend_request_data = json.loads(friend_request_json_data.decode("utf-8"))
-        friend_request_list = friend_request_data["senders"]
-        context = {'user' : user, 'username': user.username, 'friend_request': friend_request_list }
+        friend_request_list = get_friend_requests(user)
+        friends_count = get_friends_count(user)
+        context = {'user' : user, 'username': user.username,
+                   'friend_request': friend_request_list, 'friends_count': friends_count}
         return render(request, "./homepage.html", context)
     else:
         return redirect("../")
