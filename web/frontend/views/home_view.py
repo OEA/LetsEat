f522e16bccd66106916c16f9a1ca9b044f786252
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-04-13 14:03:58 +0300

    Added events to homepage

--- web/frontend/views/home_view.py
+++ web/frontend/views/home_view.py
@@ -31,6 +31,16 @@ def get_friends_count(user):
 
     return len(friend_list)
 
+def get_events(user):
+    params = urllib.parse.urlencode({'username': user.username})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "application/json"}
+    connection = http.client.HTTPConnection('127.0.0.1', 8000)
+    connection.request("POST", "/api/get_owned_events/", params, headers)
+    event_list_response = connection.getresponse()
+    event_list_json_data = event_list_response.read()
+    event_list_data = json.loads(event_list_json_data.decode("utf-8"))
+    event_list = event_list_data["events"]
+    return event_list
 
 def homepage(request):
     context = None
@@ -38,8 +48,11 @@ def homepage(request):
         user = request.user
         friend_request_list = get_friend_requests(user)
         friends_count = get_friends_count(user)
+        get_owned_events = get_events(user)
         context = {'user' : user, 'username': user.username,
-                   'friend_request': friend_request_list, 'friends_count': friends_count}
+                   'friend_request': friend_request_list,
+                   'friends_count': friends_count,
+                   'events': get_owned_events}
         return render(request, "./homepage.html", context)
     else:
         return redirect("../")
